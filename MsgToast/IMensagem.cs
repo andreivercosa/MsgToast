afc57603a04a6a3d21f678f0db937d756a30f641
andreivercosa <andreivercosa@gmail.com>
2019-02-28 20:07:57 -0400

    v1

---
+++ MsgToast/IMensagem.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MsgToast
+{
+    public interface IMensagem
+    {
+        void LongTime(string msg);
+        void ShortTime(string msg);
+    }
+}
---
+++ MsgToast/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Widget;
+using Xamarin.Forms;
+
+namespace MsgToast
+{
+    public partial class MainPage : ContentPage, IMensagem
+    {
+        void clique_longo(object sender, System.EventArgs e)
+        {
+            LongTime("Testado o tempo longo");
+        }
+
+        void clique_curto(object sender, System.EventArgs e)
+        {
+            ShortTime("Testado o tempo curto");
+        }
+
+        public void LongTime(string msg)
+        {
+            Toast.MakeText(Android.App.Application.Context,msg, ToastLength.Long).Show();
+        }
+
+        public void ShortTime(string msg)
+        {
+            Toast.MakeText(Android.App.Application.Context, msg, ToastLength.Short).Show();
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
